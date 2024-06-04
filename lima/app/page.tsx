8ff03dec0de614bb28d0278513cd2018ae8c73e9
CBF <147460707+CBF2006@users.noreply.github.com>
2024-06-04 14:39:09 -0400

    Add button?

--- lima/app/page.tsx
+++ lima/app/page.tsx
@@ -1,9 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
-      <p className="text-teal-500 font-bold">
-        Welcome to Project Lima!
-      </p>
+      <Button>Click Me</Button>
     </div>
   )
 }
