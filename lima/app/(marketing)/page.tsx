db794c1d90a4f64c0ea56c6d2075f18e711a03e3
CBF <147460707+CBF2006@users.noreply.github.com>
2024-06-04 15:14:34 -0400

    Basics of Marketing Page

--- lima/app/(marketing)/page.tsx
+++ lima/app/(marketing)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button size="lg" variant="destructive">Click Me</Button>
+      This is a marketing page
     </div>
   )
 }
