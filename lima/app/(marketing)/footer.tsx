2cb713106271507982d8521ef8cdcfb633eab7d7
CBF <147460707+CBF2006@users.noreply.github.com>
2024-06-04 20:50:39 -0400

    Adding Learn Page

--- lima/app/(marketing)/footer.tsx
+++ lima/app/(marketing)/footer.tsx
@@ -1,7 +1,31 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
     return (
         <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-            Footer
+            <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+              <Button size="lg" variant="ghost" className="w-full">
+                <Image 
+                    src="/kr.svg" 
+                    alt="Korean" 
+                    height={32} 
+                    width={40}
+                    className="mr-4 rounded-md" 
+                />
+                Korean
+              </Button>
+              <Button size="lg" variant="ghost" className="w-full">
+                <Image 
+                    src="/jp.svg" 
+                    alt="Japanese" 
+                    height={32} 
+                    width={40}
+                    className="mr-4 rounded-md" 
+                />
+                Japanese
+              </Button>
+            </div>
         </footer>
     );
 }
