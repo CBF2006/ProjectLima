a916c13df86f467f7932be402a5410cb6e6c2ae2
CBF <147460707+CBF2006@users.noreply.github.com>
2024-06-05 21:22:27 -0400

    Functional Sidebar + Logout (+redirect)

--- lima/components/sidebar.tsx
+++ lima/components/sidebar.tsx
@@ -1,4 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import {
+    ClerkLoading,
+    ClerkLoaded,
+    UserButton,
+} from "@clerk/nextjs";
+
+import { Loader } from "lucide-react";
+
 import { cn } from "@/lib/utils";
+
+import { SidebarItem } from "./sidebaritem";
 
 type Props = {
     className?: string;
@@ -7,10 +20,50 @@ type Props = {
 export const Sidebar = ({ className }: Props) => {
     return (
         <div className={cn(
-            "flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+            "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
             className,
             )}>
-            Sidebar
+            <Link href="/learn">
+                <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+                    <Image src="/sana.webp" width={40} height={40} alt="Sana" />
+                    <h1 className="text-2xl font-extrabold text-cyan-400 tracking-wide">
+                        Project Lima
+                    </h1>
+                </div>
+            </Link>
+            <div className="flex flex-col gap-y-2 flex-1">
+                <SidebarItem 
+                label="Learn" 
+                href="/learn"
+                iconSrc="/learn.svg"
+                />
+                
+                <SidebarItem 
+                label="Leaderboard" 
+                href="/leaderboard"
+                iconSrc="/leaderboard.svg"
+                />
+
+                <SidebarItem 
+                label="Quests" 
+                href="/quests"
+                iconSrc="/quests.svg"
+                />
+
+                <SidebarItem 
+                label="Shop" 
+                href="/shop"
+                iconSrc="/shop.svg"
+                />
+            </div>
+            <div className="p-4">
+                <ClerkLoading>
+                    <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+                </ClerkLoading>
+                <ClerkLoaded>
+                    <UserButton afterSignOutUrl="/" />
+                </ClerkLoaded>
+            </div>
         </div>
     );
 };
