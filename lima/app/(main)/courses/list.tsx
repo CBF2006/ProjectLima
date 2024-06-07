c1e196ab081ce5e83e52d95d9b8d16f1ed8466a9
CBF <147460707+CBF2006@users.noreply.github.com>
2024-06-06 23:02:29 -0400

    Courses Page Done!
    It looks sooooooooo good

---
+++ lima/app/(main)/courses/list.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { courses } from "@/db/schema"
+import { Card } from "./card";
+
+type Props = {
+    courses: typeof courses.$inferSelect[];
+    activeCourseID: number;
+};
+
+export const List = ({ courses, activeCourseID }: Props) => {
+    return (
+        <div className="pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))]">
+            {courses.map((course) => (
+                <Card
+                key={course.id}
+                id={course.id}
+                title={course.title}
+                imageSrc={course.imageSrc}
+                onClick={() => {}}
+                disabled={false}
+                active={course.id === activeCourseID}
+                />
+            ))}
+        </div>
+    );
+};
--- lima/app/(main)/courses/page.tsx
+++ lima/app/(main)/courses/page.tsx
@@ -1,13 +1,18 @@
 import { getCourses } from "@/db/queries";
 
+import { List } from "./list";
+
 const CoursesPage = async () => {
-    const data = await getCourses();
+    const courses = await getCourses();
     return (
         <div className="h-full max-w-[912 px] px-3 mx-auto">
             <h1 className="text-2xl font-bold text-neutral-700">
                 Language Courses
             </h1>
-            {JSON.stringify(data)}
+            <List
+            courses={courses}
+            activeCourseID={1}
+            />
         </div>
     );
 };
