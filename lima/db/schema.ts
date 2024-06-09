cc990437e5b5a1107e2b442bf05c3aff715a18e3
CBF <147460707+CBF2006@users.noreply.github.com>
2024-06-08 22:39:49 -0400

    Create userProgress Table in Drizzle

--- lima/db/schema.ts
+++ lima/db/schema.ts
@@ -1,7 +1,29 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const courses = pgTable("courses", {
     id: serial("id").primaryKey(),
     title: text("title").notNull(),
     imageSrc: text("image_src").notNull(),
 });
+
+export const coursesRelations = relations(courses, ({ many }) => ({
+    userProgress: many(userProgress),
+}));
+
+export const userProgress = pgTable("userProgress", {
+     userID: text("user_id").primaryKey(),
+     userName: text("user_name").notNull().default("User"),
+     userImageSource: text("user_image_src").notNull().default("/sana.svg"),
+     activeCourseId: integer("active_course_id").references(() => courses.id, { onDelete: "cascade"}),
+     hearts: integer("hearts").notNull().default(5),
+     points: integer("points").notNull().default(0),
+});
+
+export const userProgressRelations = relations(userProgress, ({ one }) => 
+({
+   activeCourseId: one(courses, {
+    fields: [userProgress.activeCourseId],
+    references: [courses.id],
+   }),
+}));
