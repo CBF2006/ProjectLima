3e6a6f42e68b4b243388cb8dc72b82e4e603fbce
CBF <147460707+CBF2006@users.noreply.github.com>
2024-06-08 23:18:08 -0400

    Redirect to Courses Page if Course not selected

--- lima/app/(main)/learn/page.tsx
+++ lima/app/(main)/learn/page.tsx
@@ -1,11 +1,27 @@
+import { redirect } from "next/navigation";
+
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
-import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
+import { getUserProgress } from "@/db/queries";
+
+import { Header } from "./header";
 
 // Sidebar Icon Hex: #22d3ee
 
-const LearnPage = () => {
+const LearnPage = async () => {
+    const userProgressData = getUserProgress();
+
+    const [
+        userProgress
+    ] = await Promise.all([
+        userProgressData
+    ]);
+
+    if (!userProgress || userProgress.activeCourse) {
+        redirect("/courses");
+    }
+
     return ( // Try and remove flex-row-reverse and flip sticky and feed wrapper's postion in the code
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
