4d20b0bed3b09fc42d757bf3c6c9254095fb643d
CBF <147460707+CBF2006@users.noreply.github.com>
2024-06-10 21:50:17 -0400

    Change Learn Page and Track User Progress by Course

--- lima/app/(main)/learn/page.tsx
+++ lima/app/(main)/learn/page.tsx
@@ -27,14 +27,14 @@ const LearnPage = async () => {
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
                 <UserProgress 
-                    activeCourse={{ title: "Korean", imageSrc: "kr.svg" }}
-                    hearts={5}
-                    points={100}
+                    activeCourse={userProgress.activeCourse}
+                    hearts={userProgress.hearts}
+                    points={userProgress.points}
                     hasActiveSubscription={false}
                 />
             </StickyWrapper>
             <FeedWrapper>
-                <Header title="Korean" />
+                <Header title={userProgress.activeCourse.title} />
             </FeedWrapper>
         </div>
     );
