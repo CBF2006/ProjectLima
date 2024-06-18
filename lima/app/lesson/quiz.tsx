f785a7a3c36a5a0f15c22729ab1add81adf72cd4
CBF <147460707+CBF2006@users.noreply.github.com>
2024-06-18 14:23:53 -0400

    Upgrade Next.js and start quiz

--- lima/app/lesson/quiz.tsx
+++ lima/app/lesson/quiz.tsx
@@ -25,6 +25,18 @@ export const Quiz = ({
  }: Props) => {
     const [hearts, setHearts] = useState(initialHearts);
     const [percentage, setPercentage] = useState(initialPercentage);
+    const [challenges] = useState(initialLessonChallenges);
+    const [activeIndex, setActiveIndex] = useState (() => {
+        const uncompletedIndex = challenges.findIndex((challenge) => !challenge.
+        completed);
+        return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+    })
+
+    const challenge = challenges[activeIndex];
+
+    const title = challenge.type === "ASSIST" // Will need to be modified once you add other question types to a if-then-else statement
+    ? "Select the correct meaning"
+    : challenge.question;
 
     return (
         <>
@@ -33,6 +45,18 @@ export const Quiz = ({
                 percentage={percentage}
                 hasActiveSubscription={!!userSubscription?.isActive}
             />
+            <div className="flex-1">
+                <div className="h-full flex items-center justify-center">
+                    <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
+                        <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
+                           Which of these is 아?
+                        </h1>
+                        <div>
+                            {title}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
  };
