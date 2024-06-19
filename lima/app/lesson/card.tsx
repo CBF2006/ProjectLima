ab1d39e8c88352bcf19b5c57cfe1846e121a3e36
CBF <147460707+CBF2006@users.noreply.github.com>
2024-06-18 21:18:54 -0400

    Audio plays and you can click!!!

--- lima/app/lesson/card.tsx
+++ lima/app/lesson/card.tsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
+import { useCallback } from "react";
+import { useAudio, useKey } from "react-use";
+
 import { cn } from "@/lib/utils";
 import { challenges } from "@/db/schema";
-import Image from "next/image";
 
 type Props = {
     id: number;
@@ -27,9 +30,20 @@ export const Card = ({
     disabled,
     type,
 }: Props) => {
+    const [audio, _, controls] = useAudio({ src: audioSrc || "" });
+
+    const handleClick = useCallback(() => {
+        if (disabled) return;
+
+        controls.play();
+        onClick();
+    }, [disabled, onClick, controls]);
+
+    useKey(shortcut, handleClick, {}, [handleClick]); // HANDLES PRESSING 1,2,3,4 OMGGGG YESSSSS
+
     return (
         <div
-            onClick={onClick}
+            onClick={handleClick}
             className={cn(
                 "h-full border-2 rounded-xl border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer active:border-b-2",
                 selected && "border-sky-300 bg-sky-100 hover:bg-sky-100",
@@ -39,6 +53,7 @@ export const Card = ({
                 type === "ASSIST" && "lg:p-3 w-full"
             )}
         >
+            {audio}
             {imageSrc && (
                 <div
                     className={cn(
