76392afecbdbd2ce02aa0080a56bfb19e386f8cf
CBF <147460707+CBF2006@users.noreply.github.com>
2024-06-24 16:11:04 -0400

    Allow adding to the database

---
+++ scripts/reset.ts
@@ -0,0 +1,33 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+
+import * as schema from "../db/schema";
+
+const sql = neon(process.env.DATABASE_URL!);
+
+const db = drizzle(sql, { schema });
+
+const main = async () => {
+    try {
+        console.log("Resetting Database...");
+
+        await db.delete(schema.challengeOptions);
+        await db.delete(schema.challenges);
+        await db.delete(schema.lessons);
+        await db.delete(schema.units);
+        await db.delete(schema.courses);
+        await db.delete(schema.userProgress);
+        await db.delete(schema.challengeProgress);
+        await db.delete(schema.userSubscription);
+
+        console.log("Resetting Finished!");
+    } catch (error) {
+        console.error(error);
+        throw new Error("Failed to reset the database");
+    }
+};
+
+main();
+
+// MAKE SURE TO INCLUDE A / WITH ANY SOURCE. When you don't, it will only work with the initial lesson, not the practice
