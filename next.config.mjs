8319293ce073ce36ecfc7b8f18f6304a8e6becf9
CBF <147460707+CBF2006@users.noreply.github.com>
2024-06-24 15:26:30 -0400

    Fix typo that broke the website

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     async headers() {
         return [
             {
-                soruce: "/api/(.*)",
+                source: "/api/(.*)",
                 headers: [
                     {
                         key: "Access-Control-Allow-Origin",
