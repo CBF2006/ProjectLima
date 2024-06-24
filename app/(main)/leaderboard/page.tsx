725689afda1061ed854aabf552f2df24deb7f09d
CBF <147460707+CBF2006@users.noreply.github.com>
2024-06-24 12:31:55 -0400

    Create Leaderboard Page

---
+++ app/(main)/leaderboard/page.tsx
@@ -0,0 +1,64 @@
+import { redirect } from "next/navigation";
+
+import { getUserProgress, getUserSubscription } from "@/db/queries";
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { UserProgress } from "@/components/user-progress";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import Image from "next/image";
+
+import { Items } from "./items";
+
+const ShopPage = async () => {
+    const userProgressData = getUserProgress();
+    const userSubscriptionData = getUserSubscription();
+
+    const [
+        userProgress,
+        userSubsciption,
+    ] = await Promise.all([
+        userProgressData,
+        userSubscriptionData
+    ]);
+
+    if (!userProgress || !userProgress.activeCourse) {
+        redirect("/courses");
+    }
+
+    const isPro = !!userSubsciption?.isActive; // We have to boolean-ify it twice because it could return null which would be bad
+    
+    return (
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+                <UserProgress 
+                    activeCourse={userProgress.activeCourse}
+                    hearts={userProgress.hearts}
+                    points={userProgress.points}
+                    hasActiveSubscription={isPro}
+                />
+            </StickyWrapper>
+            <FeedWrapper>
+                <div className="w-full flex flex-col items-center">
+                    <Image 
+                        src="/shop.svg"
+                        alt="Shop"
+                        height={90}
+                        width={90}
+                    />
+                    <h1 className="text-center font-bold text-neutral-800 text-2xl my-6">
+                        Shop
+                    </h1>
+                    <p className="text-muted-foreground text-center text-lg mb-6">
+                        Spend your points on cool stuff.
+                    </p>
+                    <Items 
+                        hearts={userProgress.hearts}
+                        points={userProgress.points}
+                        hasActiveSubscription={isPro}
+                    />
+                </div>
+            </FeedWrapper>
+        </div>
+    );
+};
+
+export default ShopPage;
