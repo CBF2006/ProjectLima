e191f14b7b6c91ef40ea276ac5146e9394be84a2
CBF <147460707+CBF2006@users.noreply.github.com>
2024-06-24 15:08:20 -0400

    Revert "Update app.tsx"
    This reverts commit 87b74322b56040a732a8a8473471c6ba39326323.

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -1,20 +1,13 @@
 "use client";
 
-import { Admin, ListGuesser, Resource } from "react-admin";
-import simpleRestProvider from "ra-data-simple-rest";
-
-const dataProvider = simpleRestProvider("/api");
+import { Admin } from "react-admin";
 
 export const App = () => {
     return (
-        <Admin dataProvider={dataProvider}>
-            <Resource 
-                name="courses"
-                list={ListGuesser}
-                recordRepresentation="title"
-            />
+        <Admin>
+            Admin?
         </Admin>
-    );
+    ) 
 };
 
 export default App;
