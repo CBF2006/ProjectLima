7c73f0da2938755c5ef8d37a09fb501008cf6e0a
CBF <147460707+CBF2006@users.noreply.github.com>
2024-06-24 15:38:19 -0400

    Finishing up Admin Unauthorized User Protection

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,17 @@
 import dynamic from "next/dynamic";
+import { redirect } from "next/navigation";
+
+import { getIsAdmin } from "@/lib/admin";
 
 const App = dynamic(() => import("./app"), { ssr: false });
 
-const AdminPage = () => {
+const AdminPage = async () => {
+    const isAdmin = await getIsAdmin();
+
+    if (!isAdmin) {
+        redirect("/");
+    }
+
     return (
         <App />
     );
