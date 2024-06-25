9aa8132339636eeeee5dcd04a0933bd98159ea07
CBF <147460707+CBF2006@users.noreply.github.com>
2024-06-24 22:07:17 -0400

    Update seed.ts

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -37,29 +37,29 @@ const main = async () => {
 
         await db.insert(schema.lessons).values([
             // Korean - Unit 1
-            { id: 1, unitId: 1, order: 1, title: "Basic Vowels 1" },
-            { id: 2, unitId: 1, order: 2, title: "Basic Consonants 1" },
-            { id: 3, unitId: 1, order: 3, title: "Basic Vowels 2" },
-            { id: 4, unitId: 1, order: 4, title: "Basic Consonants 2" },
-            { id: 5, unitId: 1, order: 5, title: "Basic Hangeul" },
+            { id: 1, unitId: 1, order: 1, title: "KR - Basic Vowels 1" },
+            { id: 2, unitId: 1, order: 2, title: "KR - Basic Consonants 1" },
+            { id: 3, unitId: 1, order: 3, title: "KR - Basic Vowels 2" },
+            { id: 4, unitId: 1, order: 4, title: "KR - Basic Consonants 2" },
+            { id: 5, unitId: 1, order: 5, title: "KR - Basic Hangeul" },
             // Korean - Unit 2
-            { id: 6, unitId: 2, order: 1, title: "Vowel Combinations 1" },
-            { id: 7, unitId: 2, order: 2, title: "Double Consonants" },
-            { id: 8, unitId: 2, order: 3, title: "Basic Vowels 2" },
-            { id: 9, unitId: 2, order: 4, title: "Final Consonant 1" },
-            { id: 10, unitId: 2, order: 5, title: "Final Consonant 2" },
+            { id: 6, unitId: 2, order: 1, title: "KR - Vowel Combinations 1" },
+            { id: 7, unitId: 2, order: 2, title: "KR - Double Consonants" },
+            { id: 8, unitId: 2, order: 3, title: "KR - Basic Vowels 2" },
+            { id: 9, unitId: 2, order: 4, title: "KR - Final Consonant 1" },
+            { id: 10, unitId: 2, order: 5, title: "KR - Final Consonant 2" },
             // Korean - Unit 3
-            { id: 11, unitId: 3, order: 1, title: "Greetings" },
-            { id: 12, unitId: 3, order: 2, title: "Pronouns" },
-            { id: 13, unitId: 3, order: 3, title: "Verbs 1" },
-            { id: 14, unitId: 3, order: 4, title: "Nouns 1" },
-            { id: 15, unitId: 3, order: 5, title: "Adverbs 1" },
+            { id: 11, unitId: 3, order: 1, title: "KR - Greetings" },
+            { id: 12, unitId: 3, order: 2, title: "KR - Pronouns" },
+            { id: 13, unitId: 3, order: 3, title: "KR - Verbs 1" },
+            { id: 14, unitId: 3, order: 4, title: "KR - Nouns 1" },
+            { id: 15, unitId: 3, order: 5, title: "KR - Adverbs 1" },
             // Japanese - Unit 1
-            { id: 16, unitId: 4, order: 1, title: "Hiragana 1" },
-            { id: 17, unitId: 4, order: 2, title: "Hiragana 2" },
-            { id: 18, unitId: 4, order: 3, title: "Hiragana 3" },
-            { id: 19, unitId: 4, order: 4, title: "Hiragana 4" },
-            { id: 20, unitId: 4, order: 5, title: "Hiragana 5" },
+            { id: 16, unitId: 4, order: 1, title: "JP - Hiragana 1" },
+            { id: 17, unitId: 4, order: 2, title: "JP - Hiragana 2" },
+            { id: 18, unitId: 4, order: 3, title: "JP - Hiragana 3" },
+            { id: 19, unitId: 4, order: 4, title: "JP - Hiragana 4" },
+            { id: 20, unitId: 4, order: 5, title: "JP - Hiragana 5" },
         ]);
 
         await db.insert(schema.challenges).values([ // KR - Lesson 1
