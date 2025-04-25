7ef3996af33541c5debe0af885f07d0b9711bdee
CBF <147460707+CBF2006@users.noreply.github.com>
2025-04-24 22:27:45 -0400

    Small Fix

--- components/topic-section.tsx
+++ components/topic-section.tsx
@@ -13,7 +13,7 @@ const TopicSection = ({ title, videos }: TopicSectionProps) => {
             <h2 className="text-2xl font-bold mb-4">
                 {title}
             </h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6">
                 {videos.map((video, index) => (
                     <VideoEmbed key={index} title={video.title} videoId={video.videoId} />
                 ))}
