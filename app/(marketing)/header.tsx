a7e91fd822310a9d8a893f4bf8b155e7af791995
CBF <147460707+CBF2006@users.noreply.github.com>
2025-05-05 15:45:01 -0400

    Adapt Welcome Page to Brand

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -15,9 +15,9 @@ export const Header = () => {
         <header className="h-20 w-full border-b-2 border-slate-200 px-4">
             <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
                 <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
-                    <Image src="/sana.webp" width={40} height={40} alt="Sana" />
-                    <h1 className="text-2xl font-extrabold text-cyan-400 tracking-wide">
-                        Project Lima
+                    <Image src="/mascot.svg" width={40} height={40} alt="Sana" />
+                    <h1 className="text-2xl font-extrabold text-brand-dark tracking-wide">
+                        Nami
                     </h1>
                 </div>
                 <ClerkLoading>
