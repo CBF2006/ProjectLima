25eea9a3fe558204493ec0539102f29ca84053d5
CBF <147460707+CBF2006@users.noreply.github.com>
2025-05-20 10:36:50 -0400

    Change Question Bubble to Mascot

--- app/lesson/question_bubble.tsx
+++ app/lesson/question_bubble.tsx
@@ -8,15 +8,15 @@ export const QuestionBubble = ({ question }: Props) => {
     return(
         <div className="flex items-center gap-x-4 mb-6">
             <Image 
-                src="/sana.webp"
-                alt="Sana"
+                src="/mascot.svg"
+                alt="Mascot"
                 width={60}
                 height={60}
                 className="hidden lg:block"
             />
             <Image 
-                src="/sana.webp"
-                alt="Sana"
+                src="/mascot.svg"
+                alt="Mascot"
                 width={40}
                 height={40}
                 className="block lg:hidden"
