5a3501ee51f7cf697186fa6ae071833b9333c75a
CBF <147460707+CBF2006@users.noreply.github.com>
2025-05-20 14:34:14 -0400

    Question Bubble Updated Formatting

--- app/lesson/question_bubble.tsx
+++ app/lesson/question_bubble.tsx
@@ -21,10 +21,10 @@ export const QuestionBubble = ({ question }: Props) => {
                 height={40}
                 className="block lg:hidden"
             />
-            <div className="relative py-2 px-4 border-2 rounded-xl text-sm lg:text-base">
+            <div className="relative py-2 px-4 border-2 rounded-xl text-sm lg:text-base font-semibold text-neutral-800 border-neutral-500">
                 {question}
                 <div 
-                    className="absolute -left-3 top-1/2 w-0 h-0 border-x-8 border-x-transparent border-t-8 transform -translate-y-1/2 rotate-90"
+                    className="absolute -left-3 top-1/2 w-0 h-0 border-x-8 border-x-transparent border-t-8 transform -translate-y-1/2 rotate-90 border-neutral-500"
                 />
             </div>
         </div>
